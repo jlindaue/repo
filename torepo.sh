9dfa3fe9c1ae48bb7ded3ed68ee17e66c7ef0429
Jan Lindauer <lindauer56@gmail.com>
2020-03-20 16:44:17 +0000

    Initial commit

---
+++ torepo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+#download packages inserted in file from aarch64 ca-artifacts repo and creates repo 
+
+
+for pkg in $(cat $1); do
+	wget -O ./repo/$pkg.rpm http://ca-artifacts.us.oracle.com/auto-build/epel/7/aarch64/$pkg.rpm
+done
+createrepo /root/repo/repo --update
