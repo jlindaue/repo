1127a8f9ea4cff9f0d2c215cb21b6194cc19c394
jlindaue <61438498+jlindaue@users.noreply.github.com>
2020-03-23 14:41:36 +0100

    Update torepo.sh

--- torepo.sh
+++ torepo.sh
@@ -4,7 +4,7 @@
 #download packages inserted in file from aarch64 ca-artifacts repo and creates repo 
 
 
-for pkg in $(cat $1); do
-	wget -O ./repo/$pkg.rpm http://ca-artifacts.us.oracle.com/auto-build/epel/7/aarch64/$pkg.rpm
+for pkg in $(awk '{print $1}' $1); do
+        wget -O ./repo/$pkg.rpm http://ca-artifacts.us.oracle.com/auto-build/epel/7/aarch64/$pkg.rpm
 done
 createrepo /root/repo/repo --update
