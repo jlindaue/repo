cc684b2512f2abc7844fe1ec8eacb4bdf519851e
jlindaue <61438498+jlindaue@users.noreply.github.com>
2020-03-24 15:59:30 +0100

    Update torepo.sh

--- torepo.sh
+++ torepo.sh
@@ -8,3 +8,4 @@ for pkg in $(awk '{print $1}' $1); do
         wget -O ./repo/$pkg.rpm http://ca-artifacts.us.oracle.com/auto-build/epel/7/aarch64/$pkg.rpm
 done
 createrepo /root/repo/repo --update
+yum clean metadata
