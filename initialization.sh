4fceb67fb4bd6f032cb00a1e05b7d1669267a178
jlindaue <61438498+jlindaue@users.noreply.github.com>
2020-05-05 14:24:30 +0200

    Update initialization.sh

--- initialization.sh
+++ initialization.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #set this as executable
 export https_proxy=""
-yum install createrepo vim wget
-mv /etc/yum.conf /root/repo/yum.conf.orig
-mv /etc/yum.repos.d/lxc-install.repo /root/repo/lxc-install.repo.orig
+yum install -y createrepo vim wget
 mv /etc/yum.repos.d/epel.repo /root/repo/epel.repo.orig
-mv /root/repo/yum.conf /etc/yum.conf
-mv /root/repo/lxc-install.repo /etc/yum.repos.d/lxc-install.repo
+mv /root/repo/local.repo /etc/yum.repos.d/local.repo
+mv /root/repo/epel.repo /etc/yum.repos.d/epel.repo
 mkdir /root/repo/repo
 createrepo /root/repo/repo
