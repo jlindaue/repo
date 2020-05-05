58e46d993390653a2b50aa808d50c8f520cd6a51
Your Name <you@example.com>
2020-05-05 09:35:29 +0000

    5.5.2020

--- remove.sh
+++ remove.sh
@@ -2,13 +2,10 @@
 
 
 #download packages inserted in file from aarch64 ca-artifacts repo and creates repo 
-
-
 for pkg in $(awk '{print $1}' $1); do
-        wget -O ./repo/$pkg.rpm http://ca-artifacts.us.oracle.com/auto-build/epel/7/aarch64/$pkg.rpm
+        yum remove $pkg
+	rm -f /root/repo/repo/$pkg
 done
+
 createrepo /root/repo/repo --update
 yum clean metadata
-for pkg in $(awk '{print $1}' $1); do
-        yum install $pkg
-done
