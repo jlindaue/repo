c0c00467e57860c5b7aa55961937138bd68b34e1
jlindaue <61438498+jlindaue@users.noreply.github.com>
2020-05-05 14:32:55 +0200

    Update initialization.sh

--- initialization.sh
+++ initialization.sh
@@ -7,3 +7,8 @@ mv /root/repo/local.repo /etc/yum.repos.d/local.repo
 mv /root/repo/epel.repo /etc/yum.repos.d/epel.repo
 mkdir /root/repo/repo
 createrepo /root/repo/repo
+if [[ ! -z $1 ]]; then
+  mv torepox86.sh torepo.sh
+else
+  rm torepox86.sh
+fi
